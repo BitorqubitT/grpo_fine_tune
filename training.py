1b04079337ad49045616c3c0d7844e443ae0414b
BitorqubitT <thierryars2@hotmail.com>
2025-05-31 13:15:55 +0200

    cleanup

--- training.py
+++ training.py
@@ -12,11 +12,14 @@ import numpy as np
 import torch
 from utils import get_logprobs
 import os
+from templates import SYSTEM_PROMPT, template_rs_file, CARGO_TOML_FILE
+
 os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
+
 device = "cuda" # the device to load the model onto
-model_name = "Qwen/Qwen3-0.6b"
+#model_name = "Qwen/Qwen3-0.6b"
 #model_name = "Qwen/Qwen3-1.8b"
-#model_name = "Qwen/Qwen2.5-1.5B-Instruct"
+model_name = "Qwen/Qwen2.5-1.5B-Instruct"
 
 tokenizer = AutoTokenizer.from_pretrained(model_name, extra_vocab_file="qwen_extra.tiktoken")
 
@@ -39,62 +42,12 @@ data_loader = DataLoader(dataset,
                          shuffle = True
                         )
 
-SYSTEM_PROMPT =  """You are a pragmatic Rust programmer. Given the following question do the following:
-    1. Write a Rust function to complete the task. Make the code simple and easy to understand. The code should pass `cargo build` and `cargo clippy`. Do not add a main function. Try to limit library usage to the standard library std. Respond with only the Rust function and nothing else.
-    2. Given the rust function you wrote, write unit tests for the function. The tests should be a simple line delimited list of assert! or assert_eq! statements. Make the tests simple and easy to understand. The code should pass `cargo build` and `cargo clippy` and `cargo test`. Do not add a main function or any other code. Respond with only the assert statements and nothing else. The tests should use super::*.
-
-    An example output should look like the following:
-
-    ```rust
-    /// Reasoning goes here
-    /// and can be multi-line
-    fn add_nums(x: i32, y: i32) -> i32 {
-      x + y
-    }
-
-    #[cfg(test)]
-    mod tests {
-        use super::*;
-
-        #[test]
-        fn test_add_nums() {
-            // Test adding positive numbers
-            assert_eq!(add_nums(4, 2), 6);
-            // Test adding a positive and negative number
-            assert_eq!(add_nums(4, -2), 2);
-            // Test adding two negative numbers
-            assert_eq!(add_nums(-12, -1), -13);
-        }
-    }
-    ```
-
-    Make sure to only respond with a single  ```rust``` block. The unit tests must be defined inside the mod tests {} module. Make sure to import any standard library modules that you need. Do not add a main function.
-    """
-
-template_rs_file = """
-#![allow(dead_code)]
-// {code}
-
-fn main() {
-    println!("Hello World");
-}
-"""
-
-cargo_toml_file ="""
-[package]
-name = "rust-program"
-version = "0.1.0"
-edition = "2021"
-
-[dependencies]
-"""
-
-#wandb.init(project = "llm finetune",
-#           name = f"experiment 9424",
-#           config = {
-#                    "gamma": 5,
-#                    }
-#            )
+wandb.init(project = "llm finetune",
+           name = f"experiment 9424",
+           config = {
+                    "gamma": 5,
+                    }
+            )
 
 columns = ['question',
            'generated_code',
@@ -103,11 +56,10 @@ columns = ['question',
            'asserts'
 ]
 
-#test_table = wandb.Table(columns = columns)
+test_table = wandb.Table(columns = columns)
 memory = Memory(3, 1, device, (3, 4, 2))
 x = GRPO_agent(model, tokenizer, SYSTEM_PROMPT, 3, memory)
-env = env(cargo_toml_file, template_rs_file)
-
+env = env(CARGO_TOML_FILE, template_rs_file)
 
 #TODO: Use the new get_logprobs function to get logprob
 # It doesnt use more resources than calculating it right after the model call for the query.
@@ -116,7 +68,7 @@ env = env(cargo_toml_file, template_rs_file)
 # Should be epochs
 for _ in range(1):
     for k, batch in enumerate(data_loader):
-        if k == 55:
+        if k == 30:
             break
         print("################################################")
         prompt = batch["rust_prompt"][0]
@@ -126,17 +78,15 @@ for _ in range(1):
  
         table_rows, total_rewards = process_batch_rewards(batch_rewards, prompt, action)
 
-        #wandb.log({
-        #"total_reward": np.mean(total_rewards),
-        #})
+        wandb.log({"total_reward": np.mean(total_rewards)})
      
         #answers:  human-readable text (useful for logging or reward computation)
         #model_inputs.input_ids: prompt
         #generated_ids: what the model did aka answer
         #generated_full_ids: full sequence, useful for recovering the original generation context
         
-        #for row in table_rows:
-           # test_table.add_data(*row)
+        for row in table_rows:
+            test_table.add_data(*row)
 
         #TODO: CHECK, I think we somehow reload the model with get_logprobs
 
@@ -157,7 +107,5 @@ for _ in range(1):
         # Copy to ref model
         memory.clear()
 
-
-
-    #wandb.log({"test_table": test_table})
-    #wandb.finish() 
+    wandb.log({"test_table": test_table})
+    wandb.finish() 
